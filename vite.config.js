8afe09adde35f27d4008722553174e3575b2d58f
Yafit Abaev <yafitabaev@Yafits-MBP.lan>
2026-06-06 18:23:55 +0300

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/MyPortfolio/",
+  base: "/My_Portfolio/",
   plugins: [react()],
  
 })
